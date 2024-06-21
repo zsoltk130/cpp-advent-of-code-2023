00314784c337ca651505f56270afacfc998de014
zsoltk130 <Zsolt@DESKTOP-VLPBAOH>
2024-06-21 23:46:49 +0100

    Use regex to extract numbers from file into a struct

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <regex>
 
 struct Race
 {
@@ -31,10 +32,40 @@ std::vector<std::string> readLinesFromFile(const std::string& filename)
     return lines;
 }
 
+std::vector<int> extractNumbersFromString(const std::string& string)
+{
+    std::vector<int> vec;
+    std::regex numberPattern("\\d+");
+
+    std::string line = string;
+    auto iterator_begin = std::sregex_iterator(line.begin(), line.end(), numberPattern);
+    auto iterator_end = std::sregex_iterator();
+
+    for (auto it = iterator_begin; it != iterator_end; ++it)
+    {
+        std::smatch match = *it;
+        std::string currentNumber = match.str();
+
+        vec.push_back(std::stoi(currentNumber));
+    }
+
+    return vec;
+}
+
+Race populateStruct(const std::vector<std::string>& lines)
+{
+    Race race;
+    race.times = extractNumbersFromString(lines[0]);
+    race.distances = extractNumbersFromString(lines[1]);
+
+    return race;
+}
+
 int main()
 {
     std::string testfile = "test.txt";
     std::vector<std::string> lines = readLinesFromFile(testfile);
+    Race race = populateStruct(lines);
 
     return 0;
 }
